1ae5aebb9977ba78640890b0dbdc08d4764a18ac
nacho robledo <nacho17insti@gmail.com>
2024-12-09 20:55:44 +0100

    Añadir método dividir a Calculadora

--- calculadora/calculadora.java
+++ calculadora/calculadora.java
@@ -10,4 +10,13 @@ public class Calculadora {
     public int multiplicar(int a, int b) {
         return a * b;
     }
+    public int dividir(int a, int b) {
+        if (b != 0) {
+            return a / b;
+        } else {
+            throw new ArithmeticException("No se puede dividir por cero");
+        }
+    }
+    
+
 }
